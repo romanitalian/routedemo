d42573ce5e6853aed709e0289efad0b637f6f103
romanitalian <romanitalian.net@gmail.com>
2016-12-02 13:04:58 +0300

    move index.php to folder with htaccess

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+error_reporting(E_ALL);
+
+include __DIR__.'/vendor/autoload.php';
+
+use Phroute\Phroute\RouteCollector;
+// use Phroute\Phroute\Dispatcher;
+
+$router = new RouteCollector();
+$router->any('/', function(){
+    return 'This responds to the default route';
+});
+
+$router->any('/example', function(){
+    return 'Tasdfasdfe';
+});
+
