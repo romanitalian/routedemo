821c541e311320366f755153fae48ccc0cefafe6
romanitalian <romanitalian.net@gmail.com>
2016-12-02 13:12:21 +0300

    test

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <?php
 error_reporting(E_ALL);
 
-include __DIR__.'/vendor/autoload.php';
+// include __DIR__.'/vendor/autoload.php';
 
-use Phroute\Phroute\RouteCollector;
-// use Phroute\Phroute\Dispatcher;
+// use Phroute\Phroute\RouteCollector;
+// // use Phroute\Phroute\Dispatcher;
 
-$router = new RouteCollector();
-$router->any('/', function(){
-    return 'This responds to the default route';
-});
+// $router = new RouteCollector();
+// $router->any('/', function(){
+//     return 'This responds to the default route';
+// });
 
-$router->any('/example', function(){
-    return 'Tasdfasdfe';
-});
+// $router->any('/example', function(){
+//     return 'Tasdfasdfe';
+// });
 
+var_dump('This is index.php');
